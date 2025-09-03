0db0b2b38216aeead031d15103af7b536df85f54
ealtun21 <ealtun21@ku.edu.tr>
2025-09-03 12:21:08 +0300

    Added invalid_username

--- src/handlers/page_detection.rs
+++ src/handlers/page_detection.rs
@@ -8,3 +8,9 @@ pub fn is_input_visible(tab: &Tab, selector: &str) -> anyhow::Result<bool> {
     );
     Ok(tab.evaluate(&js, false)?.value.unwrap().as_bool().unwrap())
 }
+
+/// Checks if the invalid username error message is visible.
+pub fn is_invalid_username_visible(tab: &Tab) -> anyhow::Result<bool> {
+    let js = "!!(document.getElementById('usernameError') && document.getElementById('usernameError').innerText.includes('We couldn\\'t find an account with that username.'))";
+    Ok(tab.evaluate(js, false)?.value.unwrap().as_bool().unwrap())
+}
