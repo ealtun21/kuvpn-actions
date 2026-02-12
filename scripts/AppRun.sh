fa22045bdb4f6f698df1938a4f6a2b19d0108cbb
ealtun21 <ealtun21@ku.edu.tr>
2026-02-12 17:14:15 +0300

    Aggressively bundle GLib and Ayatana stack to fix runtime symbol errors

--- scripts/AppRun.sh
+++ scripts/AppRun.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-HERE="$(dirname "$(readlink -f "${0}")")"
-export PATH="${HERE}/usr/bin:${PATH}"
-export LD_LIBRARY_PATH="${HERE}/usr/lib:${LD_LIBRARY_PATH}"
-export KUVPN_CHROME_PATH="${HERE}/usr/lib/chromium/chrome"
+export SELF=$(readlink -f "$0")
+export HERE="${SELF%/*}"
 
+export PATH="${HERE}/usr/bin:${PATH}"
+export LD_LIBRARY_PATH="${HERE}/usr/lib:${HERE}/usr/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}"
+export XDG_DATA_DIRS="${HERE}/usr/share:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+
+# Chrome path for full version
+export KUVPN_CHROME_PATH="${HERE}/usr/lib/chromium/chrome"
 if [ ! -f "$KUVPN_CHROME_PATH" ]; then
     unset KUVPN_CHROME_PATH
 fi
 
-exec kuvpn-gui "$@"
+# Run the app
+exec "${HERE}/usr/bin/kuvpn-gui" "$@"
